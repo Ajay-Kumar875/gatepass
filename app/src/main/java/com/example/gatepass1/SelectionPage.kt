3a01a3e616ddcab6af924ffbed0b9e6354538526
Ajay-Kumar875 <ajaypadedam789@gmail.com>
2023-04-20 19:48:40 +0530

    finished project

---
+++ app/src/main/java/com/example/gatepass1/SelectionPage.kt
@@ -0,0 +1,11 @@
+package com.example.gatepass1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection_page)
+    }
+}
